6b90ec55654515407b8e056e45985f99f6d2032c
Diego Costa <diegodcsouza@hotmail.com>
2018-09-03 22:41:47 -0300

    Add project files.

---
+++ DemoCrud2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoCrud2.Startup))]
+namespace DemoCrud2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
